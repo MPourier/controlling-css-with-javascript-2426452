e429fd8132b721676f195fda3ef56fb1a0bedbfb
MPourier <146856052+MPourier@users.noreply.github.com>
2024-04-30 19:22:31 +0000

    JS change to CSS styling

--- 03_06b/script.js
+++ 03_06b/script.js
@@ -1,3 +1,8 @@
 const styleSheet = document.styleSheets[0];
 
 console.log(styleSheet.cssRules[2]);
+
+let maxInlineValue = styleSheet.cssRules[2].styleMap.get("max-inline-size").value;
+let maxInlineUnit = styleSheet.cssRules[2].styleMap.get("max-inline-size").unit;
+
+console.log('maxInlineValue = ${maxInlineValue}, maxInlineUnit = ${maxInlineUnit}'); 
